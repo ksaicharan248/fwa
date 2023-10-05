8021bb2e3b13759c6a408b43ca1e0dedee2864c9
Sai charan <saicharanreddy11102003@gmail.com>
2023-10-05 20:18:36 +0530

    new update

--- COC.py
+++ COC.py
@@ -1,5 +1,6 @@
 import requests
 from setkey import auth
+import random
 
 header = {'Accept' : 'application/json' , 'Authorization' : auth}
 
@@ -12,13 +13,19 @@ def verify(player , token) :
     url = f'https://api.clashofclans.com/v1/players/%23{player}/verifytoken'
     response = requests.post(url , headers=verifyheaders , json=data)
     s = response.json()
-    return response.status_code,s
+    return response.status_code , s
 
 
 def get_user(tag) :
     reso = requests.get('https://api.clashofclans.com/v1/players/%23' + tag , headers=header)
     userinfo = reso.json()
     return userinfo
+
+
+def getclan(tag) :
+    resp = requests.get('https://api.clashofclans.com/v1/clans/%23' + tag , headers=header)
+    claninfo = resp.json()
+    return claninfo
 
 
 def get_id(th) :
@@ -30,18 +37,24 @@ def get_id(th) :
         return "1157933611337666620"
     elif th == 14 :
         return "1157934828784734299"
-    else:
+    else :
         return "1158776040525680694"
 
+def get_random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    hex_color = f'#{r:02X}{g:02X}{b:02X}'
+    return hex_color
 
-def get_role(role:str) :
+def get_role(role: str) :
     if role == "leader" :
         return "Leader"
     elif role == "coLeader" :
         return "Co-Leader"
-    elif role == "admin":
+    elif role == "admin" :
         return "Elder"
-    else:
+    else :
         return "Member"
 
 
