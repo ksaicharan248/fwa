6055930282a1f8d05ef1af8019b386ef26eb2400
EC2 Default User <ec2-user@ip-172-31-17-210.ap-southeast-2.compute.internal>
2024-01-14 10:42:31 +0000

    small updates

--- dataadder.py
+++ dataadder.py
@@ -1,6 +1,17 @@
 import pickle
 
-with open("ex.pkl" , "rb") as file :
-    user_data = pickle.load(file)
+with open('userdata.pkl' , 'rb') as f :
+    token = pickle.load(f)
 
-print(user_data)
+for user in token.keys() :
+    data = token[user]
+    token[user]['tag'] = data['tag'].strip('#')
+    token[user]['clan'] = data['clan'].strip('#')
+
+with open("userdata.pkl" , "wb") as file :
+    pickle.dump(token , file)
+
+with open('userdata.pkl' , 'rb') as f :
+    data = pickle.load(f)
+
+print(data)
